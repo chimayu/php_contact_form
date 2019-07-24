c3e56da076e0fa7c5d1ebdf5ddc491478dc468ec
mayumi <mayumi@asadigital.net>
2019-07-24 11:41:27 +0800

    入力画面とチェック画面作成

---
+++ check.php
@@ -0,0 +1,20 @@
+<?php
+    $nickname = $_POST['nickname'];
+    echo $nickname . '<br>';
+
+    $email = $_POST['email'];
+    echo $email . '<br>';
+
+    $content = $_POST['content'];
+    echo $content;
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>入力内容確認</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>入力内容確認</h1>
+</body>
+</html>
