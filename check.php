c9012441fdcbc024dfe79f35a891308102fbf4c5
mayumi <mayumi@asadigital.net>
2019-07-25 10:32:30 +0800

    送信ボタンなど実装

--- check.php
+++ check.php
@@ -1,12 +1,26 @@
 <?php
     $nickname = $_POST['nickname'];
-    echo $nickname . '<br>';
-
     $email = $_POST['email'];
-    echo $email . '<br>';
-
     $content = $_POST['content'];
-    echo $content;
+
+    if ($nickname == '') {
+        $nickname_result = 'ニックネームが入力されていません。';
+    } else {
+        $nickname_result = 'ようこそ、' . $nickname .'様';
+    }
+    
+    if ($email == '') {
+        $email_result = 'メールアドレスが入力されていません。';
+    } else {
+        $email_result = 'メールアドレス：' . $email;
+    }
+
+    if ($content == '') {
+        $content_result =  'お問い合わせ内容が入力されていません。';
+    } else {
+        $content_result = 'お問い合わせ内容：' . $content;
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -16,5 +30,18 @@
 </head>
 <body>
     <h1>入力内容確認</h1>
+    <p><?php echo $nickname_result; ?></p>
+    <p><?php echo $email_result; ?></p>
+    <p><?php echo $content_result; ?></p>
+    <!-- 送信内容が入ってない場合はOKボタンを出さないPHP -->
+    <form method="POST" action="thanks.php">
+        <input type="hidden" name="nickname" value="<?php echo $nickname; ?>">
+        <input type="hidden" name="email" value="<?php echo $email; ?>">
+        <input type="hidden" name="content" value="<?php echo $content; ?>">
+        <input type="button" value="戻る" onclick="history.back()">
+        <?php if ($nickname != '' && $email != '' && $content != ''): ?>
+            <input type="submit" value="OK">
+        <?php endif; ?>
+    </form>
 </body>
 </html>
